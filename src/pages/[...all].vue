ca91ab6a6287c3705d2e20d174c65caf9dbfe44b
besscroft <besscroft@foxmail.com>
2023-02-18 21:00:51 +0800

    :rocket:迁移博客首页仓库

---
+++ src/pages/[...all].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <!-- 404 page content -->
+  </div>
+</template>
+<script setup lang="ts">
+onMounted(() => {
+  // 404 page script
+  let nav = document.createElement('script')
+  nav.type = 'text/javascript'
+  nav.src = '//qzonestyle.gtimg.cn/qzone/hybrid/app/404/search_children.js'
+  document.body.appendChild(nav)
+})
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { isDark } from '~/composables'
+
+defineOptions({
+  name: 'IndexPage',
+})
+</script>
+
+<template>
+  <div class="mt-20">
+    <div class="m-auto w-40"><img class="rounded-full" src="https://besscroft.com/uploads/avatar.jpeg" alt="Bess Croft"></div>
+    <p class="m-2">
+      <a rel="noreferrer" class="font-serif text-3xl" href="https://github.com/besscroft" target="_blank">
+        Bess Croft
+      </a>
+    </p>
+    <p>
+      <em text-sm op75 class="font-serif">只要不失去你的崇高，整个世界都会为你敞开！</em>
+    </p>
+
+    <div py-4 />
+    <div>
+      <a
+        class="inline-block rounded-full bg-gradient-to-r p-[2px] hover:text-white focus:outline-none focus:ring active:text-opacity-75"
+        target="blank" href="https://github.com/besscroft">
+        <img src="https://img.shields.io/badge/GitHub-Follow-brightgreen?style=flat-square" alt="GitHub">
+      </a>
+    </div>
+    <div class="mt-4">
+      <a
+        class="inline-block rounded-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-[2px] hover:text-white focus:outline-none focus:ring active:text-opacity-75"
+        href="https://blog.besscroft.com"
+      >
+        <span
+          v-if="isDark"
+          class="block rounded-full bg-black px-8 py-3 text-sm font-medium hover:bg-transparent font-serif"
+        >
+          博客
+        </span>
+        <span
+          v-else
+          class="block rounded-full bg-white px-8 py-3 text-sm font-medium hover:bg-transparent font-serif"
+        >
+          博客
+        </span>
+      </a>
+    </div>
+  </div>
+</template>
