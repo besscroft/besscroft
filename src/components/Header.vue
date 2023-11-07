0fed38d95a73004ac846da9b5e4c1908ce2b5fb2
besscroft <besscroft@foxmail.com>
2023-11-07 13:30:20 +0800

    add kamera url

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,6 +8,9 @@ const router = useRouter()
     <a href="https://blog.besscroft.com" target="_blank" title="博客">
       <div i="carbon-blog" />
     </a>
+    <a href="https://kamera.heming.dev/" target="_blank" title="旅行足迹">
+      <div i="carbon-image-reference" />
+    </a>
     <div cursor-pointer i="carbon-bot" @click="router.push('/about')" title="关于" />
     <a href="https://github.com/besscroft" target="_blank" title="GitHub">
       <div i="carbon-logo-github" />
